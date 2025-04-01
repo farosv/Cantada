f030088caf4eca633851504770e88f6a26288463
farosv <113446994+farosv@users.noreply.github.com>
2025-04-01 13:20:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function cantadaCerta() {
-    document.getElementById("popupCerto").style.display = "flex";
-}
-function cantadaErrada() {
-    document.getElementById("popupErrado").style.display = "flex";
-}
-function fecharPopup(id) {
-    document.getElementById(id).style.display = "none";
-}
